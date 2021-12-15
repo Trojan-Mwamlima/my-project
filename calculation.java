06bc142718541f3e81a36239446441f8ab17f884
Trojan Mwamlima <bed-com-45-19@unima.ac.ac.mw>
2021-12-15 11:14:11 -0800

    added license file

--- calculation.java
+++ calculation.java
@@ -1,25 +1,5 @@
-import java.util.Scanner;
-//create a class
 public class calculation{
-    //main method
     public static void main( String[] args){
         System.out.println("the area will be shown here");
-
-        Scanner input = new Scanner(System.in);
-
-       //prompt the user to enter length
-        System.out.println("enter length");
-        int length = input.nextInt();
-         
-         //prompt the user to enter breadth
-           System.out.println("enter breadth");
-        int breadth = input.nextInt();
-
-        //create object
-    Area area1 = new Area();
-
-area1.setArea(length, breadth);
-//returns area
-  System.out.println("the area is "+ area1.getArea());
     }
 }
